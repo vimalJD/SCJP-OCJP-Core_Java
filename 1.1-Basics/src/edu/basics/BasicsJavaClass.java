83bf4074ebe7f21bae60cb52dffa6f3f164ebd5e
vimalJD <vimal929@gmail.com>
2025-04-16 01:12:21 +0530

    created project and simple class

---
+++ 1.1-Basics/src/edu/basics/BasicsJavaClass.java
@@ -0,0 +1,15 @@
+package edu.basics;
+
+public class BasicsJavaClass {
+	public static void main(String[] args) {
+		
+		int firstNumb = 10;
+		int secondNumb = 3;
+		int divisionResult;
+		
+		divisionResult = firstNumb / secondNumb;
+		
+		System.out.println("divisionResult: " +divisionResult);
+			
+	}
+}
